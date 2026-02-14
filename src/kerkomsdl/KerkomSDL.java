3993c878c5d480444f5249f9cdd1fda855d0905a
asus <asus@LAURA-ANGELINA>
2026-02-14 19:29:19 +0700

    First Commit

--- src/kerkomsdl/KerkomSDL.java
+++ src/kerkomsdl/KerkomSDL.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package kerkomsdl;
 
-/**
- *
- * @author asus
- */
 public class KerkomSDL {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
+         // a. Deklarasi array integer sebanyak 10
+        int[] angka = {1,2,3,4,5,6,7,8,9,10};
+
+        int jumlah = 0;
+
+        // b. Menghitung jumlah
+        for(int i = 0; i < angka.length; i++) {
+            jumlah += angka[i];
+        }
+
     }
     
 }
